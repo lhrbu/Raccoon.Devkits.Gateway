b9faa7f5895f4fb0acd0205ffbf61223218c4a55
lhrbuxiaoxin <lihaoran228@163.com>
2021-02-09 00:09:30 +0800

    Pick interface of IGatewayAddressProvider

---
+++ Raccoon.Devkits.Gateway.Client/IGatewayAddressProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Raccoon.Devkits.Gateway.Client
+{
+    public interface IGatewayAddressProvider
+    {
+        ValueTask<Uri> GetAsync(string gatewayServiceName = "Gateway", Uri? consulUri = null);
+    }
+}
